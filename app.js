83ce94e1fc0797b91a166730ee06e2f223f6ee9f
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-11-09 17:53:33 -0800

    Make certain fields required

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const promptUser = () => {
     {
       type: 'input',
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubInput => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Enter your github username!");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
@@ -35,12 +51,28 @@ Add a New Project
     {
       type: 'input',
       name: 'name',
-      message: 'What is the name of your project?'
+      message: 'What is the name of your project? (Required)',
+      validate: inputName => {
+        if (inputName) {
+          return true;
+        } else {
+          console.log("Enter the project name");
+          return false;
+        }
+      }
     },
     {
       type: 'input',
       name: 'description',
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)',
+      validate: inputDescription => {
+        if (inputDescription) {
+          return true;
+        } else {
+          console.log("Enter a project description");
+          return false;
+        }
+      }
     },
     {
       type: 'checkbox',
@@ -51,7 +83,15 @@ Add a New Project
     {
       type: 'input',
       name: 'link',
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: inputLink => {
+        if (inputLink) {
+          return true;
+        } else {
+          console.log("Enter the GitHub link to your project");
+          return false;
+        }
+      }
     },
     {
       type: 'confirm',
